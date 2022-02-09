3e4998f947e865a9a54d57dc0b2c03c65c73ed7c
Akanksha Vardani <akanksha.vardani@gmail.com>
2022-02-09 12:30:21 -0500

    Initial questions to answer in the data

--- affidavit_data_explore.R
+++ affidavit_data_explore.R
@@ -26,3 +26,8 @@ outpath <- "~/Desktop/Research/Projects/affidavit_data-/data_analysis/"
 
 ## Loading the affidavit data
 affidavit <- read.csv(paste0(inpath_shrug,"affidavits/shrug-v1.5.samosa-affidavits-csv/affidavits_clean.csv"))
+
+## What do I need to find in the data first -
+# How to know which candidate is for state and which for general elections - especially if both elections in the same year
+# How many candidates repeat over time - to get a sense on incumbency 
+# what all the variables mean 
