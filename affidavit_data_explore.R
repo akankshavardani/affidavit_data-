3e5a8eaec434b92352f44e21e733cf1e2e26f836
Akanksha Vardani <akanksha.vardani@gmail.com>
2022-02-09 12:20:02 -0500

    packages and specifiying path names

--- affidavit_data_explore.R
+++ affidavit_data_explore.R
@@ -1,3 +1,7 @@
 ############ Project : Why do corrupt politicians get elected ################
 ## Author : Akanksha Vardani
 ## Start Date : 9 February 2022
+
+## Setting the data upload and save paths
+inpath_shrug <- "~/Dropbox/Datasets/SHRUG/"
+outpath <- "/Users/akankshavardani/Desktop/Research/Projects/affidavit_data-/data_analysis/"
